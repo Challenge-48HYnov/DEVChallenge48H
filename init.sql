81dedeb9adca9bc84cda595a684ae76206f02e00
tatdorian <144996289+tatdorian@users.noreply.github.com>
2026-03-30 13:47:27 +0200

    Initialize database and create tables for project

---
+++ init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS projet48h;
+USE projet48h;
+
+
+CREATE TABLE IF NOT EXISTS Localisation (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pays VARCHAR(100) NOT NULL,
+    ville VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Indice (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    indice FLOAT NOT NULL,
+    localisation_id INT NOT NULL,
+    date DATETIME NOT NULL,
+    CONSTRAINT fk_localisation
+        FOREIGN KEY (localisation_id) 
+        REFERENCES Localisation(id)
+        ON DELETE CASCADE
+);
